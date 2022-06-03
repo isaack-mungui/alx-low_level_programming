3e875ab3af43fccf75e7c7d71b632bcf73b74d0e
isaack-mungui <munguiisaack@gmail.com>
2022-06-03 05:12:51 +0300

    Cleaned up code by renaming variables

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, message, sizeof(message));
+	write(2, msg, sizeof(msg));
 	return (1);
 }
