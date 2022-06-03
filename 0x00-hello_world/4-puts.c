53edf425fc886ca9229088372527a2cf79a46f27
isaack-mungui <munguiisaack@gmail.com>
2022-06-03 03:33:24 +0300

    Added fix to script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(int ac, char **av)
+int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle \n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
 
