ba7b01af901aaa74347d046faaaacf5ff0fdc1ad
isaack-mungui <munguiisaack@gmail.com>
2022-06-03 05:02:43 +0300

    Added script to print text followed by a new line

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, sizeof(message));
+	return (1);
+}
