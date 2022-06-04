4536bde4c94790cbbce63a2f7f3ec61a6cce3b47
isaack-mungui <munguiisaack@gmail.com>
2022-06-04 07:22:32 +0300

    Added description to match betty documentation style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,10 @@
 
 /*
  * main: main block
+ * description: Assign a random number to the 
+ * variable n each time it is executed. Print
+ * whether the number stored in the variable n
+ * is positive or negative.
  * Return: 0
  */
 
