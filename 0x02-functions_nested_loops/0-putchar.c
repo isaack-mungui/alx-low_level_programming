d1bdac9d93329d204bda94df230e3b3b23752051
isaack-mungui <munguiisaack@gmail.com>
2022-06-07 18:06:59 +0300

    Added folder and program to print '_putchar'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Prints _putchar,
+ * followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
