b8716b98be4ac5b67f4dc4a13316242bd7ebafa0
isaack-mungui <munguiisaack@gmail.com>
2022-06-07 18:40:42 +0300

    Modified program and defined header files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
 	return (0);
 }
