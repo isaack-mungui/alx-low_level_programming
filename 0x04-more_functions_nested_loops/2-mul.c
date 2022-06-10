48677a84f40f22c5278be69b390329c84c9af31a
isaack-mungui <munguiisaack@gmail.com>
2022-06-10 05:10:01 +0300

    Added function that maltuplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - returns the product of its parameters
+ * @a: number of type int
+ * @b: number of type int
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
