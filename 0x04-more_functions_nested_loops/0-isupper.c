d92c75eebeeae5f27eafedb437d8dba3d807baf5
isaack-mungui <munguiisaack@gmail.com>
2022-06-10 04:51:38 +0300

    Added function to check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for uppercase letter
+ * @c: character to check
+ * Return: 0 | 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
