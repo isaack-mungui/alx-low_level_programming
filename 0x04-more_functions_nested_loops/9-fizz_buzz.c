bb4147025b4b24a55d5a17f72ff686a58b73c534
isaack-mungui <munguiisaack@gmail.com>
2022-06-10 17:06:12 +0300

    Added minor changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,18 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-		else
+		else 
 			printf("%d ", i);
+
+		if (i != 100)
+			putchar(' ');
 	}
 
+	putchar('\n');
 	return (0);
 }
