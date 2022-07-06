6d99600c362d786fbf6e077c2f0f969998b251ce
isaack-mungui <munguiisaack@gmail.com>
2022-07-06 04:46:59 +0300

    Added a header file that defines a macro named PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * file: 1-pi.h
+ * Description: Defines a macro named PI
+ * as an abbreviation for the token
+ * 3.1415926535359
+ */
+
+#define PI 3.14159265359
+
+#endif
