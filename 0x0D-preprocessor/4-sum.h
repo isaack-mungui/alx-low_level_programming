98bbc842c89b078cafe69ea84baafaa02269704e
isaack-mungui <munguiisaack@gmail.com>
2022-07-06 05:18:58 +0300

    Added a function-like macro that computes the sum of two numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Description: computes the sum
+ * of x and y
+ * @x: first integer
+ * @y: second integer
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
