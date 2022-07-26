fc6c181907b0de9046e071d5298c96fe31af1bc4
isaack-mungui <munguiisaack@gmail.com>
2022-07-26 04:32:22 +0300

    Fixed Betty documentation to clear warnings

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/*
- * create_file: creates a file.
+/**
+ * create_file - creates a file.
  * @filename: name of the file to create.
  * @text_content: NULL terminated string to
- * 	 	  write to the file.
+ * write to the file.
  * Return: 1 on success
- * 	   -1 on failure 	 	  
+ * -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (o == -1 || w == -1)
 		return (-1);
 
-	close (o);
+	close(o);
 
 	return (1);
 }
