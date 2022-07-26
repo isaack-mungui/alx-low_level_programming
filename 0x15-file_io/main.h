d4d75afdaa792916bca925292fb875a3ae9d2a33
isaack-mungui <munguiisaack@gmail.com>
2022-07-26 04:25:56 +0300

    Added the create_file function prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,1 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc: Header file containint function prototypes.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endif
